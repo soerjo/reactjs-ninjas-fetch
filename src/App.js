beba222dcd56905b59f67baa203850c152ab4ea8
soerjo <ryohastomo@gmail.com>
2021-04-04 23:24:11 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>belajar Reactjs ninjas Fetch by useEffect</h3>
+      <hr />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Table() {
+  return (
+    <div>
+      <h3>isi table</h3>
+    </div>
+  );
+}
+
+export default Table;
